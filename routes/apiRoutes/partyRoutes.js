2f8869dcef42fa09e854513a2d81985c581a9854
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-08-18 16:26:24 -0400

    get vote totals

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
 
-
 // GET all parties
 router.get('/parties', (req, res) => {
     const sql = `SELECT * FROM parties`;
